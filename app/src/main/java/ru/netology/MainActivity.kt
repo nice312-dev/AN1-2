db33a42f9f7b6dd8dc48975695abab75df9b4502
Ilhom <nice.512@mail.ru>
2021-02-23 14:56:03 +0500

    first commit

---
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.netology
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println(R.string.hello)
+        println(getString(R.string.hello))
+    }
+}
